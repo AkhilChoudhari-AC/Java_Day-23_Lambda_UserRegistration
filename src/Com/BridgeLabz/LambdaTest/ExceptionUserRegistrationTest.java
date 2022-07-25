461dfcacbafcf315acbaac1d860499a577b21010
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-25 21:23:02 +0530

    ADDED: Added the user Email ID

--- src/Com/BridgeLabz/LambdaTest/ExceptionUserRegistrationTest.java
+++ src/Com/BridgeLabz/LambdaTest/ExceptionUserRegistrationTest.java
@@ -4,21 +4,40 @@ import Com.BridgeLabz.LambdaUserRegistration.ExceptionUserRegistration;
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * testCases for validating lastName
- *
- */
 public class ExceptionUserRegistrationTest {
     ExceptionUserRegistration exceptionUserRegistration = new ExceptionUserRegistration();
+
+    /**
+     * testCases for validating firstName,lastName and emailId
+     *
+     */
+    @Test
+    /**
+     * created method givenFirstName_IsProper_ReturnTrue() for true condition
+     */
+    public void givenFirstName_IsProper_ReturnTrue() {
+        /**
+         * calling firstName method to see firstname is valid and result will be in
+         * boolean type
+         */
+        boolean actualResult = exceptionUserRegistration.firstName("Akhil");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    /**
+     * created method givenFirstName_IsProper_ReturnFalse() for false condition
+     */
+    public void givenFirstName_IsProper_ReturnFalse() {
+        boolean actualResult = exceptionUserRegistration.firstName("akhil");
+        Assert.assertEquals(false, actualResult);
+    }
 
     @Test
     /**
      * created method givenLastName_IsProper_ReturnTrue() for true condition
      */
     public void givenLastName_IsProper_ReturnTrue() {
-        /**
-         * calling lastName method to see lastname is valid or not
-         */
         boolean actualResult = exceptionUserRegistration.lastName("Choudhari");
         Assert.assertEquals(true, actualResult);
     }
@@ -29,6 +48,24 @@ public class ExceptionUserRegistrationTest {
      */
     public void givenLastName_IsProper_ReturnFalse() {
         boolean actualResult = exceptionUserRegistration.lastName("choudhari");
+        Assert.assertEquals(false, actualResult);
+    }
+
+    @Test
+    /**
+     * created method givenEmailId_IsProper_ReturnTrue() for true condition
+     */
+    public void givenEmailId_IsProper_ReturnTrue() {
+        boolean actualResult = exceptionUserRegistration.email("akhil.xxxxx@gmail.com");
+        Assert.assertEquals(true, actualResult);
+    }
+
+    @Test
+    /**
+     * created method givenEmailId_IsNotProper_ReturnFalse() for false condition
+     */
+    public void givenEmailId_IsNotProper_ReturnFalse() {
+        boolean actualResult = exceptionUserRegistration.email("akhil.xxxyy.gmail.com");
         Assert.assertEquals(false, actualResult);
     }
 }
--- src/Com/BridgeLabz/LambdaUserRegistration/ExceptionUserRegistration.java
+++ src/Com/BridgeLabz/LambdaUserRegistration/ExceptionUserRegistration.java
@@ -3,8 +3,32 @@ package Com.BridgeLabz.LambdaUserRegistration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * UC1-As a User need to enter a valid First Name - First name starts with Cap
+ * and has minimum 3 characters UC2-As a User need to enter a valid Last Name -
+ * Last name starts with Cap and has minimum 3 characters UC3-As a User need to
+ * enter a valid email
+ *
+ * @author user-Almas
+ *
+ */
 public class ExceptionUserRegistration {
 
+    /**
+     * created method firstName and passing parameter in this it will check
+     * FirstName is valid or not result will be in true or false because method is
+     * boolean type
+     *
+     * @param str -passing string of first name
+     * @return -return to method created
+     */
+    public boolean firstName(String str) {
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+
+    // validating last name
     /**
      * created method lastName and passing parameter in this it will check FirstName
      * is valid or not result will be in true or false because method is boolean
@@ -25,6 +49,21 @@ public class ExceptionUserRegistration {
          * we can use matcher object to match the given pattern in the target string we
          * can create matcher object by using matcher() of pattern class
          */
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+
+    // validating email
+    /**
+     * created method email and passing parameter in this it will check email is
+     * valid or not result will be in true or false because method is boolean type
+     *
+     * @param str -passing email id
+     * @return -return to method created
+     */
+    public boolean email(String str) {
+        Pattern pattern = Pattern
+                .compile("^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+([.][a-zA-Z]{2,4})*$");
         Matcher matcher = pattern.matcher(str);
         return matcher.matches();
     }
